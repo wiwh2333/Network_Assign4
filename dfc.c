baef987c52a4d23445b5c19f33527d63db9f5a10
wiwh2333 <91509145+wiwh2333@users.noreply.github.com>
2024-05-04 14:29:15 -0600

    Add files via upload

--- dfc.c
+++ dfc.c
@@ -19,11 +19,13 @@ FILE *file;
 long long result;
 long long store;
 int size;
+int not_work=0;
 char command[100];
-char chunk1[1000];
-char chunk2[1000];
-char chunk3[1000];
-char chunk4[1000];
+char chunk1[100000];
+char chunk2[100000];
+char chunk3[100000];
+char chunk4[100000];
+char list1[10000], list2[10000],list3[10000],list4[10000];
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex3 = PTHREAD_MUTEX_INITIALIZER;
@@ -40,7 +42,7 @@ int main(int argc , char *argv[])
 	/* 
    * check command line arguments 
    */
-  	if (argc != 3) {
+  	if (argc < 2) {
     	fprintf(stderr, "usage: %s <command> <filename> \n", argv[0]);
     	exit(1);
   	}
@@ -76,10 +78,10 @@ void *connection_handler(void *server_number){
     int number = *(int*) server_number;
     portno=10000 + number;
     int first_chunk, second_chunk, chunk_size, small_result, chunker;
-    char buffer[10000], buffer2[10000];
+    char buffer[100000], buffer2[100000];
+
     size_t bytes_read;
 
-    printf("Enter Func%d\n\r",number);
     switch(result){
         case 0:
             first_chunk = number%4;
@@ -101,7 +103,7 @@ void *connection_handler(void *server_number){
             printf("ERROR WRONG HASH OF FILENAME");
             break;
     }
-    printf("NUMBER: %d My Chunks are %d and %d",number, first_chunk, second_chunk);
+    //printf("NUMBER: %d My Chunks are %d and %d",number, first_chunk, second_chunk);
     //Create socket
 	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
 	if (socket_desc == -1)
@@ -119,7 +121,13 @@ void *connection_handler(void *server_number){
     bcopy((char *)server_name->h_addr, (char *)&server.sin_addr.s_addr, server_name->h_length);
     //Connect 
     if(connect(socket_desc, (struct sockaddr *)&server, sizeof(server))<0){
-        perror("Connect failed");
+        //perror("Connect failed");
+        pthread_mutex_lock(&mutex1);
+        not_work++;
+        pthread_mutex_unlock(&mutex1);
+        printf("Connect Failed");
+        close(socket_desc);
+        return;
     }
     //Send command and filename(hashed)
     if((send(socket_desc, &command, sizeof(command), 0)) == -1){perror("Error Sending Data");}
@@ -133,6 +141,7 @@ void *connection_handler(void *server_number){
         }
 
     if (strcasecmp(command, "put") ==0){
+        
         if((send(socket_desc, &store, sizeof(store), 0)) ==-1){perror("Error Sending Data");}
         // // Receive ACK
         if ((bytes_read = recv(socket_desc, buffer, sizeof(buffer), 0)) < 0) {
@@ -147,7 +156,7 @@ void *connection_handler(void *server_number){
         fseek(file, first_chunk,SEEK_SET);
         if((bytes_read = fread(buffer, 1, chunk_size, file))<=0){perror("Error Reading File");}
         rewind(file);
-        printf("Chunk%d,Buf%d,1st%d,number%d\n", chunk_size,bytes_read,first_chunk,number);
+        //printf("Chunk%d,Buf%ld,1st%d,number%d\n", chunk_size,bytes_read,first_chunk,number);
         if((send(socket_desc, buffer, bytes_read, 0)) != bytes_read){perror("Error Sending Data");}
         for (volatile int i = 0; i < 9999; i++){ if (i%3==0){ i++;}}
         chunk_size = size/4;
@@ -157,11 +166,7 @@ void *connection_handler(void *server_number){
         fseek(file, second_chunk,SEEK_SET);
         if((bytes_read = fread(buffer, 1, chunk_size, file))<=0){perror("Error Reading File");}
         rewind(file);
-        // // // Receive ACK
-        // if ((bytes_read = recv(socket_desc, buffer, sizeof(buffer), 0)) < 0) {
-        //     perror("recv");
-        // }
-        printf("Chunk2 %d\n", chunk_size);
+        //printf("Chunk2 %d\n", chunk_size);
         if((send(socket_desc, buffer, bytes_read, 0)) != bytes_read){perror("Error Sending Data");}
         //fclose(file);
     }
@@ -171,61 +176,95 @@ void *connection_handler(void *server_number){
         if ((bytes_read = recv(socket_desc, buffer, sizeof(buffer), 0)) < 0) {
             perror("recv");
         }
-        printf("GOT\n");
+        //printf("GOT\n");
         //ACK
         if (send(socket_desc, "Ack", sizeof("Ack"), 0) == -1) {
             perror("Error sending data");
             exit(EXIT_FAILURE);
         }
-        printf("ACK\n");
+        //printf("ACK\n");
         // Receive the 2nd message
         if ((bytes_read = recv(socket_desc, buffer2, sizeof(buffer2), 0)) < 0) {
             perror("recv");
         }
-        printf("TOM\n");
+        //printf("TOM\n");
         fflush(stdout);
         switch(first_chunk){
             case 0:
                 pthread_mutex_lock(&mutex1);
-                memcpy(chunk1,buffer,1000);
+                memcpy(chunk1,buffer,100000);
                 pthread_mutex_unlock(&mutex1);
                 break;
             case 1:
                 pthread_mutex_lock(&mutex2);
-                memcpy(chunk2,buffer,1000);
+                memcpy(chunk2,buffer,100000);
                 pthread_mutex_unlock(&mutex2);
                 break;
             case 2:
                 pthread_mutex_lock(&mutex3);
-                memcpy(chunk3,buffer,1000);
+                memcpy(chunk3,buffer,100000);
                 pthread_mutex_unlock(&mutex3);
                 break;
             case 3:
                 pthread_mutex_lock(&mutex4);
-                memcpy(chunk4,buffer,1000);
+                memcpy(chunk4,buffer,100000);
                 pthread_mutex_unlock(&mutex4);
                 break;
         }
         switch(second_chunk){
             case 0:
                 pthread_mutex_lock(&mutex1);
-                memcpy(chunk1,buffer2,1000);
+                memcpy(chunk1,buffer2,100000);
                 pthread_mutex_unlock(&mutex1);
                 break;
             case 1:
                 pthread_mutex_lock(&mutex2);
-                memcpy(chunk2,buffer2,1000);
+                memcpy(chunk2,buffer2,100000);
                 pthread_mutex_unlock(&mutex2);
                 break;
             case 2:
                 pthread_mutex_lock(&mutex3);
-                memcpy(chunk3,buffer2,1000);
+                memcpy(chunk3,buffer2,100000);
                 pthread_mutex_unlock(&mutex3);
                 break;
             case 3:
                 pthread_mutex_lock(&mutex4);
-                memcpy(chunk4,buffer2,1000);
+                memcpy(chunk4,buffer2,100000);
                 pthread_mutex_unlock(&mutex4);
+                break;
+        }
+    }
+    else if (strcasecmp(command, "list") ==0){
+        //printf("Listing");
+        //ACK
+        if (send(socket_desc, "Ack", sizeof("Ack"), 0) == -1) {
+            perror("Error sending data");
+            exit(EXIT_FAILURE);
+        }
+        switch(number){
+            case 1:
+                // Receive the 1st message
+                if ((bytes_read = recv(socket_desc, list1, sizeof(list1), 0)) < 0) {
+                    perror("recv");
+                }
+                break;
+            case 2:
+                // Receive the 1st message
+                if ((bytes_read = recv(socket_desc, list2, sizeof(list2), 0)) < 0) {
+                    perror("recv");
+                }
+                break;
+            case 3:
+                // Receive the 1st message
+                if ((bytes_read = recv(socket_desc, list3, sizeof(list3), 0)) < 0) {
+                    perror("recv");
+                }
+                break;
+            case 4:
+                // Receive the 1st message
+                if ((bytes_read = recv(socket_desc, list4, sizeof(list4), 0)) < 0) {
+                    perror("recv");
+                }
                 break;
         }
     }
@@ -253,7 +292,7 @@ void put_command(char* filename){
     compute_md5_hash(filename,&md5);
     result = md5%4; if (result < 0){result = result * -1;}
     store = md5;
-    printf("Result of %lld %% 4 = %lld,%d,%d\n", md5, result,size,size%4);
+    printf("Result of %lld %% 4 = %lld\n", md5, result);
 
     //Call Threads an Send
     pthread_t dfs1,dfs2,dfs3,dfs4;		
@@ -276,6 +315,33 @@ void put_command(char* filename){
 	{
 		perror("could not create dfs4");
 	}
+
+        //Join
+    // Join dfs1
+if (pthread_join(dfs1, NULL) != 0) {
+    perror("pthread_join failed for dfs1");
+}
+
+// Join dfs2
+if (pthread_join(dfs2, NULL) != 0) {
+    perror("pthread_join failed for dfs2");
+}
+
+// Join dfs3
+if (pthread_join(dfs3, NULL) != 0) {
+    perror("pthread_join failed for dfs3");
+}
+
+// Join dfs4
+if (pthread_join(dfs4, NULL) != 0) {
+    perror("pthread_join failed for dfs4");
+}
+    if (not_work >= 2) {
+        printf("%s put failed\n",filename);
+        //return;
+    }
+
+
 }
 void get_command(char* filename){
     int cur_pos;
@@ -318,6 +384,9 @@ void get_command(char* filename){
 	{
 		perror("could not create dfs4");
 	}
+
+
+    
     //Join
     // Join dfs1
 if (pthread_join(dfs1, NULL) != 0) {
@@ -339,30 +408,33 @@ if (pthread_join(dfs4, NULL) != 0) {
     perror("pthread_join failed for dfs4");
 }
 
-    
+    if (not_work >= 2) {
+        printf("%s is Incomplete\n",filename);
+        return;
+    }
     snprintf(store_name, sizeof(store_name), "%lld", store);
     // Open the output file in binary write mode
     // for (size_t i = 0; i < 100; i++) {
     //     printf("%02X ", (unsigned char)chunk1[i]);
     // }
-    printf("\n");
-    printf("Chunk1%s\n",chunk1);
-    printf("Chunk2%s\n",chunk2);
-    printf("Chunk3%s\n",chunk3);
-    printf("Chunk4%s\n",chunk4);
-    fflush(stdout);
+    // printf("\n");
+    // printf("Chunk1%s\n",chunk1);
+    // printf("Chunk2%s\n",chunk2);
+    // printf("Chunk3%s\n",chunk3);
+    // printf("Chunk4%s\n",chunk4);
+    //fflush(stdout);
     FILE* output = fopen(store_name, "wb");
      // Write chunk1 to the output file
-    fwrite(chunk1, 1, 9, output);
+    fwrite(chunk1, 1, size/4, output);
 
     // Write chunk2 to the output file
-    fwrite(chunk2, 1, 9, output);
+    fwrite(chunk2, 1, size/4, output);
 
     // Write chunk2 to the output file
-    fwrite(chunk3, 1, 9, output);
+    fwrite(chunk3, 1, size/4, output);
 
     // Write chunk2 to the output file
-    fwrite(chunk4, 1, 9, output);
+    fwrite(chunk4, 1, (size/4)+(size%4), output);
 
     // Close the output file
     fclose(output);
@@ -370,7 +442,54 @@ if (pthread_join(dfs4, NULL) != 0) {
 
 }
 void list_command(){
+    //Call Threads an Send
+    pthread_t dfs1,dfs2,dfs3,dfs4;		
+    int *dfc1,*dfc2,*dfc3,*dfc4;
+    dfc1 = malloc(1); dfc2 = malloc(1);dfc3 = malloc(1);dfc4 = malloc(1);
+    *dfc1=1;*dfc2=2;*dfc3=3;*dfc4=4;
+	if( pthread_create(&dfs1, NULL ,  connection_handler,(void *)dfc1) < 0)
+	{
+		perror("could not create dfs1");
+	}
+    if( pthread_create(&dfs2, NULL ,  connection_handler,(void *)dfc2) < 0)
+	{
+		perror("could not create dfs2");
+	}
+    if( pthread_create(&dfs3, NULL ,  connection_handler,(void *)dfc3) < 0)
+	{
+		perror("could not create dfs3");
+	}
+    if( pthread_create(&dfs4, NULL ,  connection_handler,(void *)dfc4) < 0)
+	{
+		perror("could not create dfs4");
+	}
+    for (volatile int i = 0; i < 9999; i++){ if (i%3==0){ i++;}}
+           //Join
+    // Join dfs1
+if (pthread_join(dfs1, NULL) != 0) {
+    perror("pthread_join failed for dfs1");
+}
 
+// Join dfs2
+if (pthread_join(dfs2, NULL) != 0) {
+    perror("pthread_join failed for dfs2");
+}
+
+// Join dfs3
+if (pthread_join(dfs3, NULL) != 0) {
+    perror("pthread_join failed for dfs3");
+}
+
+// Join dfs4
+if (pthread_join(dfs4, NULL) != 0) {
+    perror("pthread_join failed for dfs4");
+}
+    if (not_work >= 2) {
+        printf("[INCOMPELTE]");
+        return;
+    }
+printf("List:%s",list1);
+return;
 }
 
 void compute_md5_hash(const char *input, unsigned long long *md5_hash) {
--- dfs.c
+++ dfs.c
@@ -12,6 +12,7 @@
 #include <dirent.h> //For directories
 #include <errno.h>
 #include <sys/stat.h> //mkdir()
+#include <sys/types.h>
 
 //the thread function
 void *connection_handler(void *);
@@ -146,8 +147,8 @@ void *connection_handler(void *socket_desc)
 
 void put_command(int sock){
 	int read_size_1,read_size_2,read_size_3;
-	char second_message[2000];
-    char third_message[2000];
+	char second_message[100000];
+    char third_message[100000];
 	char folder_name[50]; // Adjust size as needed
 	char second_file_path[100]; // Adjust size as needed
 	char third_file_path[100]; // Adjust size as needed
@@ -167,7 +168,7 @@ void put_command(int sock){
         if ((read_size_2 = recv(sock, second_message, sizeof(second_message), 0)) < 0) {
             perror("recv");
         }
-		for (volatile int i = 0; i < 99999; i++){ if (i%3==0){ i++;}}
+		for (volatile int i = 0; i < 2000000; i++){ if (i%3==0){ i++;}}
         // Receive the third message
         if ((read_size_3 = recv(sock, third_message, sizeof(third_message), 0)) < 0) {
             perror("recv");
@@ -203,7 +204,14 @@ void put_command(int sock){
 	fflush(stdout);
 	snprintf(folder_name, sizeof(folder_name), "dfs%d/%lld",number, first_message);
     if (mkdir(folder_name, 0777) != 0) {
-        perror("mkdir");
+        //perror("mkdir");
+		DIR *dir = opendir(folder_name);
+        if (dir != NULL) {
+            closedir(dir);
+        } else {
+            perror("opendir failed");
+            return;
+        }
     }
 
 	snprintf(second_file_path, sizeof(second_file_path), "%s/file1.bin", folder_name);
@@ -226,9 +234,10 @@ void put_command(int sock){
     }
     if (fwrite(third_message, 1, read_size_3, file2) != read_size_3) {
     perror("fwrite");
-    fclose(file1);
+    fclose(file2);
 	}
     fclose(file2);
+	printf("End");
 }
 
 void get_command(int sock){
@@ -237,7 +246,7 @@ void get_command(int sock){
 	long long first_message;
 	long long result;
 	char hash[21];
-	char buffer[10000];
+	char buffer[1000000];
 	// Receive the first message (assuming it's an integer)
         if ((read_size_1 = recv(sock, &first_message, sizeof(long long), 0)) < 0) {
             perror("recv");
@@ -319,5 +328,33 @@ void get_command(int sock){
 }
 
 void list_command(int sock){
+	DIR *dir;
+    struct dirent *entry;
+    char folders[10000] = "";
+	char foldername[100];
+	char buffer[110];
+	sprintf(foldername, "dfs%d/", number);
 
+	// Open the current directory
+    dir = opendir(foldername);
+    if (dir == NULL) {
+        perror("Unable to open directory");
+        return;
+    }
+	while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == 4) { // Check if it's a directory
+            strcat(folders, entry->d_name); // Add folder name to the string
+            strcat(folders, "\n"); // Add newline for readability
+        }
+    }
+	printf("Listing%s",folders);
+	// Receive ACK
+	size_t bytes_read;
+        if ((bytes_read = recv(sock, buffer, sizeof(buffer), 0)) < 0) {
+            perror("recv");
+        }
+	if (send(sock, folders, sizeof(folders), 0) == -1) {
+            perror("Error sending data");
+            exit(EXIT_FAILURE);
+        }
 }
